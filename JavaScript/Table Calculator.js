e29856bd78900d4df9a0a31ee87203cb4271bef0
Popstar Idhant <81918779+Idhant-6@users.noreply.github.com>
2022-09-30 23:21:33 +0530

    Create Table Calculator.js

---
+++ JavaScript/Table Calculator.js
@@ -0,0 +1,11 @@
+let num = window.prompt("Tell me the number for which you want the table.");
+document.writeln(`${num} x 1 = ${num * 1}<br>`);
+document.writeln(`${num} x 2 = ${num * 2}<br>`);
+document.writeln(`${num} x 3 = ${num * 3}<br>`);
+document.writeln(`${num} x 4 = ${num * 4}<br>`);
+document.writeln(`${num} x 5 = ${num * 5}<br>`);
+document.writeln(`${num} x 6 = ${num * 6}<br>`);
+document.writeln(`${num} x 7 = ${num * 7}<br>`);
+document.writeln(`${num} x 8 = ${num * 8}<br>`);
+document.writeln(`${num} x 9 = ${num * 9}<br>`);
+document.writeln(`${num} x 10 = ${num * 10}<br>`);
